b4adef26f58bd2d217e38c364a58726b7e325a91
sweglord227 <sweglord227@gmail.com>
2024-01-31 12:12:16 -0500

    resync

--- nvim/lua/config/keybinds.lua
+++ nvim/lua/config/keybinds.lua
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Tabs
@@ -5,3 +6,18 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- vim.keymap.set("n", "<C-i>", vim.tabprevious)
 -- vim.keymap.set("n", "<C-o>", vim.tabnext)
 -- vim.keymap.set("n", "<C-p>", vim.tabclose)
+=======
+local map = vim.keymap
+local cmd = vim.cmd
+map.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Tabs
+map.set("n", "<C-u>", cmd.tabnew)
+map.set("n", "<C-i>", cmd.tabprevious)
+map.set("n", "<C-o>", cmd.tabnext)
+map.set("n", "<C-p>", cmd.tabclose)
+
+-- Folds
+map.set("n", "<leader>j", "zM")
+map.set("n", "<leader>k", "zR")
+>>>>>>> 991f3f7 (resync)
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -32,4 +32,9 @@ opt.foldexpr = "nvim_treesitter#foldexpr()"
 
 opt.foldcolumn = '1'
 
+<<<<<<< HEAD
+=======
+opt.foldlevel = 99
+
+>>>>>>> 991f3f7 (resync)
 -- opt.fillchars = 
