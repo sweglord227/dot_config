f6c96b2f1aeffd2743614bf52cc74529a9c8cbc2
sweglord227 <sweglord227@gmail.com>
2024-04-16 14:18:45 -0400

    added undotree plugin

--- nvim/lua/config/keybinds.lua
+++ nvim/lua/config/keybinds.lua
@@ -1,12 +1,11 @@
-
 local map = vim.keymap
 local cmd = vim.cmd
 
 -- Tabs
-map.set("n", "<C-u>", cmd.tabnew)
-map.set("n", "<C-i>", cmd.tabprevious)
-map.set("n", "<C-o>", cmd.tabnext)
-map.set("n", "<C-p>", cmd.tabclose)
+-- map.set("n", "<C-u>", cmd.tabnew)
+-- map.set("n", "<C-i>", cmd.tabprevious)
+-- map.set("n", "<C-o>", cmd.tabnext)
+-- map.set("n", "<C-p>", cmd.tabclose)
 
 -- Folds
 map.set("n", "<leader>j", "zM")
@@ -18,6 +17,14 @@ map.set('n', '<leader>pf', builtin.find_files, {})
 map.set('n', '<leader>pg', builtin.live_grep, {})
 map.set('n', '<leader>pb', builtin.buffers, {})
 map.set('n', '<leader>ph', builtin.help_tags, {})
+
+-- UndoTree
+local function toggle_undotree ()
+        cmd.UndotreeToggle()
+        cmd.UndotreeFocus()
+end
+
+map.set('n', '<C-u>', toggle_undotree)
 
 -- Oil File Manager
 map.set("n", "<leader>pv", vim.cmd.Oil)
